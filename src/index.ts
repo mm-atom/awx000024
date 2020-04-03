e752ba18bcf81c289a30693456341232158eaa55
taoqf <tao_qiufeng@126.com>
2020-04-03 13:03:41 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+declare const global: { mm: object; };
 
-export default function awx24(param1: string, param2: string) {
+export default function set<T>(key: string, value: T) {
+	const mm = global.mm ? global.mm : (global.mm = {});
+	mm[key] = value;
 }
